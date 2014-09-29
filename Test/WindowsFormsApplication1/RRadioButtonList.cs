4155733cc874ce843fb539b11f9fb24ace1cb2dc
Pavel0786 <Pavel0786@yahoo.com>
2014-09-29 21:11:51 +0600

    My first commit

---
+++ Test/WindowsFormsApplication1/RRadioButtonList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class RRadioButtonList : CheckedListBox
+    {
+        public RRadioButtonList()
+        {
+            InitializeComponent();
+
+        }
+
+        public RRadioButtonList(IContainer container)
+        {
+            
+        }
+
+        public virtual void OnItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked)
+                for (int ix = 0; ix < this.Items.Count; ++ix)
+                    if (e.Index != ix) 
+                        this.SetItemChecked(ix, false);
+        }
+    }
+}
